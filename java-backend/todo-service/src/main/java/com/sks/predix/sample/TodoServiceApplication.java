4f5e5e304fb73d7d16cdfd55e8dbca113b7abca5
Sabith <sabith@ge.com>
2015-11-21 15:28:37 -0800

    Added configuration required to protect the routes on todo-server

--- java-backend/todo-service/src/main/java/com/sks/predix/sample/TodoServiceApplication.java
+++ java-backend/todo-service/src/main/java/com/sks/predix/sample/TodoServiceApplication.java
@@ -2,11 +2,28 @@ package com.sks.predix.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @SpringBootApplication
-public class TodoServiceApplication {
+@EnableResourceServer
+public class TodoServiceApplication extends ResourceServerConfigurerAdapter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TodoServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TodoServiceApplication.class, args);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/todo/**").authorizeRequests().anyRequest()
+				.authenticated();
+	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources)
+			throws Exception {
+		resources.resourceId("openid");
+	}
 }
--- java-backend/todo-service/src/main/java/com/sks/predix/sample/entity/repository/TodoRepository.java
+++ java-backend/todo-service/src/main/java/com/sks/predix/sample/entity/repository/TodoRepository.java
@@ -1,7 +1,9 @@
-package com.sks.predix.sample.entity;
+package com.sks.predix.sample.entity.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.sks.predix.sample.entity.Todo;
 
 @RestResource(path = "/todo")
 public interface TodoRepository extends CrudRepository<Todo, Long> {
